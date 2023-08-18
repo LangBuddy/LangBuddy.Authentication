66387d2d7adb84cd73706eb757f0c8acd2d14e19
FremyXS <antonovch13@mail.ru>
2023-08-18 19:43:39 +0300

    feat(Service): add command VerifyPasswordHashCommand

---
+++ LangBuddy.Authentication.Service/Authentication/Commands/VerifyPasswordHashCommand.cs
@@ -0,0 +1,39 @@
+﻿using LangBuddy.Authentication.Service.Authentication.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LangBuddy.Authentication.Service.Authentication.Commands
+{
+    public class VerifyPasswordHashCommand : IVerifyPasswordHashCommand
+    {
+        public bool Invoke(string password, byte[] storedHash, byte[] storedSalt)
+        {
+            if (password == null)
+                throw new ArgumentNullException("password");
+
+            if (string.IsNullOrWhiteSpace(password))
+                throw new ArgumentException("Value cannot be empty or whitespace only string.", "password");
+
+            if (storedHash.Length != 64)
+                throw new ArgumentException("Invalid length of password hash (64 bytes expected).", "passwordHash");
+
+            if (storedSalt.Length != 128)
+                throw new ArgumentException("Invalid length of password salt (128 bytes expected).", "passwordSalt");
+
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(storedSalt))
+            {
+                var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+                for (int i = 0; i < computedHash.Length; i++)
+                {
+                    if (computedHash[i] != storedHash[i])
+                        return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
