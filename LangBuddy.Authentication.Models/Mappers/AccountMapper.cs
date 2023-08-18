7d860cea87c64fdad68cb963ec224c4fc667535a
FremyXS <antonovch13@mail.ru>
2023-08-18 18:55:39 +0300

    feat(Models): add mapper AccountMapper

---
+++ LangBuddy.Authentication.Models/Mappers/AccountMapper.cs
@@ -0,0 +1,19 @@
+﻿using LangBuddy.Authentication.Models.Request;
+using Mapster;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LangBuddy.Authentication.Models.Mappers
+{
+    public static class AccountMapper
+    {
+        public static AccountCreateRequest ToAccountCreateRequest(this AuthCreateRequest request)
+        {
+            var req = request.Adapt<AccountCreateRequest>();
+            return request.Adapt<AccountCreateRequest>();
+        }
+    }
+}
