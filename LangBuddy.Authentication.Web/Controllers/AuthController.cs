95f68afab312f26945ec1e0ef4f1c5d4de66e927
FremyXS <antonovch13@mail.ru>
2023-08-18 22:33:09 +0300

    feat(Web): add controller AuthController

---
+++ LangBuddy.Authentication.Web/Controllers/AuthController.cs
@@ -0,0 +1,44 @@
+﻿using LangBuddy.Authentication.Models.Request;
+using LangBuddy.Authentication.Service.Authentication.Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LangBuddy.Authentication.Web.Controllers
+{
+    [Route("[controller]")]
+    public class AuthController: ControllerBase
+    {
+        private readonly IAuthenticationService _authenticationService;
+        public AuthController(IAuthenticationService authenticationService) 
+        {
+            _authenticationService = authenticationService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] AuthRegisterRequest authCreateRequest)
+        {
+            try
+            {
+                var res = await _authenticationService.Register(authCreateRequest);
+                return Ok(res);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] AuthLoginRequest authLoginRequest)
+        {
+            try
+            {
+                var res = await _authenticationService.Authenticate(authLoginRequest);
+                return Ok(res);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
