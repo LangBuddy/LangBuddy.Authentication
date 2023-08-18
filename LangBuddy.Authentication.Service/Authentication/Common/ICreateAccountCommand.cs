cd15dd63af9d63f2362923eddb5b3efebde05873
FremyXS <antonovch13@mail.ru>
2023-08-18 19:03:38 +0300

    refactor(Service): remove unused imports

--- LangBuddy.Authentication.Service/Authentication/Common/ICreateAccountCommand.cs
+++ LangBuddy.Authentication.Service/Authentication/Common/ICreateAccountCommand.cs
@@ -1,9 +1,4 @@
 ﻿using LangBuddy.Authentication.Models.Request;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LangBuddy.Authentication.Service.Authentication.Common
 {
