78e9249463e7356318b8e8e66e707bd79e8847dc
FremyXS <antonovch13@mail.ru>
2023-08-18 18:59:51 +0300

    feat(Service): add command CreateAccountCommand

---
+++ LangBuddy.Authentication.Service/Authentication/Commands/CreateAccountCommand.cs
@@ -0,0 +1,32 @@
+﻿using LangBuddy.Authentication.Models.Mappers;
+using LangBuddy.Authentication.Models.Request;
+using LangBuddy.Authentication.Service.Authentication.Common;
+using LangBuddy.Authentication.Service.Http.Common;
+
+namespace LangBuddy.Authentication.Service.Authentication.Commands
+{
+    public class CreateAccountCommand: ICreateAccountCommand
+    {
+        private readonly ICreatePasswordHashCommand _createPasswordHashCommand;
+        private readonly IHttpService _httpService;
+
+        public CreateAccountCommand(ICreatePasswordHashCommand createPasswordHashCommand,
+            IHttpService httpService)
+        {
+            _createPasswordHashCommand = createPasswordHashCommand;
+            _httpService = httpService;
+        }
+        public async Task<HttpContent> Invoke(AuthCreateRequest authCreateRequest)
+        {
+            byte[] passwordHash, passwordSalt;
+            _createPasswordHashCommand.Invoke(authCreateRequest.Password, out passwordHash, out passwordSalt);
+
+            var account = authCreateRequest.ToAccountCreateRequest();
+
+            account.PasswordHash = passwordHash;
+            account.PasswordSalt = passwordSalt;
+
+            return await _httpService.SendCreateAccountRequest(account);
+        }
+    }
+}
---
+++ LangBuddy.Authentication.Service/Authentication/Common/ICreateAccountCommand.cs
@@ -0,0 +1,14 @@
+﻿using LangBuddy.Authentication.Models.Request;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LangBuddy.Authentication.Service.Authentication.Common
+{
+    public interface ICreateAccountCommand
+    {
+        Task<HttpContent> Invoke(AuthCreateRequest authCreateRequest);
+    }
+}
