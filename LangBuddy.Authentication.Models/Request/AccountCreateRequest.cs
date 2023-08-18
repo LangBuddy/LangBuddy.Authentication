8bff994501b14d2da1fa46b489db80e5e0acb713
FremyXS <antonovch13@mail.ru>
2023-08-18 18:55:20 +0300

    refactor(Models): change AccountCreateRequest

--- LangBuddy.Authentication.Models/Request/AccountCreateRequest.cs
+++ LangBuddy.Authentication.Models/Request/AccountCreateRequest.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace LangBuddy.Authentication.Models.Request
 {
-    public record AccountCreateRequest(
-        string Email, string Nickname, byte[] PasswordSalt, byte[] PasswordHash
-    );
+    public record AccountCreateRequest
+    {
+        public string Email { get; set; }
+        public string Nickname { get; set; } 
+        public byte[] PasswordSalt { get; set; }
+        public byte[] PasswordHash { get; set; }
+    }
 }
