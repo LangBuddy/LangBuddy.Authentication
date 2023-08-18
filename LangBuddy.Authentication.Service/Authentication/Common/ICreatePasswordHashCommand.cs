d03a43a2dc4a00307d931402822c61c3255d3684
FremyXS <antonovch13@mail.ru>
2023-08-18 18:59:32 +0300

    feat(Service): add command CreatePasswordHashCommand

---
+++ LangBuddy.Authentication.Service/Authentication/Common/ICreatePasswordHashCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LangBuddy.Authentication.Service.Authentication.Common
+{
+    public interface ICreatePasswordHashCommand
+    {
+        void Invoke(string password, out byte[] passwordHash, out byte[] passwordSalt);
+    }
+}
