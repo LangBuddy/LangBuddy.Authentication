49aa6c0bec6ae00506da6c12d07b2b4f62072edc
FremyXS <antonovch13@mail.ru>
2023-08-18 18:55:02 +0300

    feat(Models): add option ApiConnections

---
+++ LangBuddy.Authentication.Models/Options/ApiConnections.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LangBuddy.Authentication.Models.Options
+{
+    public class ApiConnections
+    {
+        public string AccountsConnection { get; set; }
+    }
+}
