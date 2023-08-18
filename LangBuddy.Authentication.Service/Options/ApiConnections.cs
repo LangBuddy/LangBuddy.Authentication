016e83e32764b33a3efd02f32ee37d18fbbb3e62
FremyXS <antonovch13@mail.ru>
2023-08-18 22:07:33 +0300

    refactore(ApiConnections): change location
    move from Models to Service

---
+++ LangBuddy.Authentication.Service/Options/ApiConnections.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LangBuddy.Authentication.Service.Options
+{
+    public class ApiConnections
+    {
+        public string AccountsConnectionDefault { get; set; }
+        public string AccountsConnectionDefaultPasswordHash { get; set; }
+    }
+}
