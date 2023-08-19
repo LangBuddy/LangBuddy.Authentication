2fa168692f8e212e73e4b278ecd520282cc517a8
FremyXS <antonovch13@mail.ru>
2023-08-19 23:57:24 +0300

    feat(Service): add command CreateRefreshTokenCommand

---
+++ LangBuddy.Authentication.Service/Authentication/Commands/CreateRefreshTokenCommand.cs
@@ -0,0 +1,18 @@
+﻿using LangBuddy.Authentication.Service.Authentication.Common;
+using System.Security.Cryptography;
+
+namespace LangBuddy.Authentication.Service.Authentication.Commands
+{
+    public class CreateRefreshTokenCommand: ICreateRefreshTokenCommand
+    {
+        public string Invoke()
+        {
+            var randomNumber = new byte[32];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(randomNumber);
+                return Convert.ToBase64String(randomNumber);
+            }
+        }
+    }
+}
