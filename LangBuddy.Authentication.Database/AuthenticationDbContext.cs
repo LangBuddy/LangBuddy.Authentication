c5ebad42be41b4715010e6f810afb482dbe0fec4
FremyXS <antonovch13@mail.ru>
2023-08-20 18:11:25 +0300

    feat(Database): add dbcontext AuthenticationDbContext

---
+++ LangBuddy.Authentication.Database/AuthenticationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LangBuddy.Authentication.Database
+{
+    public class AuthenticationDbContext: DbContext
+    {
+        public DbSet<Entity.Authentication> Authentications { get; set; }
+
+        public AuthenticationDbContext(DbContextOptions<AuthenticationDbContext> options): base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
