cb47e2fbe17190b6ee6683c2e8d138cbbe430849
FremyXS <antonovch13@mail.ru>
2023-08-20 18:11:42 +0300

    feat(Database): add entity Authentication

---
+++ LangBuddy.Authentication.Database/Entity/Authentication.cs
@@ -0,0 +1,10 @@
+﻿namespace LangBuddy.Authentication.Database.Entity
+{
+    public class Authentication
+    {
+        public long Id { get; set; }
+        public string Email { get; set; }
+        public string? RefreshToken { get; set; }
+        public DateTime RefreshTokenExpiryTime { get; set; }
+    }
+}
