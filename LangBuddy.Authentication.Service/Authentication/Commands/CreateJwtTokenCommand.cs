a8347e9b807909c3e4f50abbabae8e110fdb81a1
FremyXS <antonovch13@mail.ru>
2023-08-20 22:03:44 +0300

    refactor(Service): change command CreateJwtTokenCommand, add override fucntion Invoke

--- LangBuddy.Authentication.Service/Authentication/Commands/CreateJwtTokenCommand.cs
+++ LangBuddy.Authentication.Service/Authentication/Commands/CreateJwtTokenCommand.cs
@@ -16,6 +16,26 @@ namespace LangBuddy.Authentication.Service.Authentication.Commands
             _jwtConfiguration = jwtConfiguration.Value;
         }
 
+        public string Invoke(IEnumerable<Claim> claims)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Issuer = _jwtConfiguration.ISSUER,
+                Audience = _jwtConfiguration.AUDIENCE,
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(
+                    _jwtConfiguration.GetSymmetricSecurityKey(),
+                    SecurityAlgorithms.HmacSha256Signature
+                )
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+
+            return tokenString;
+        }
+
         public string Invoke(string accountEmail, string pasHash)
         {
             var tokenHandler = new JwtSecurityTokenHandler();
--- LangBuddy.Authentication.Service/Authentication/Common/ICreateJwtTokenCommand.cs
+++ LangBuddy.Authentication.Service/Authentication/Common/ICreateJwtTokenCommand.cs
@@ -1,7 +1,10 @@
-﻿namespace LangBuddy.Authentication.Service.Authentication.Common
+﻿using System.Security.Claims;
+
+namespace LangBuddy.Authentication.Service.Authentication.Common
 {
     public interface ICreateJwtTokenCommand
     {
         string Invoke(string accountEmail, string pasHash);
+        string Invoke(IEnumerable<Claim> claims);
     }
 }
