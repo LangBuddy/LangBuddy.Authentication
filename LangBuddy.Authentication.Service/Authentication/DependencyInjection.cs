7df39be4e97384aa209a6606ccb1a93a02d471b5
FremyXS <antonovch13@mail.ru>
2023-08-20 22:12:11 +0300

    refactor(Service): add DependencyInjection to authentication service

---
+++ LangBuddy.Authentication.Service/Authentication/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using LangBuddy.Authentication.Service.Authentication.Commands;
+using LangBuddy.Authentication.Service.Authentication.Common;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LangBuddy.Authentication.Service.Authentication
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddAuthenticationService(this IServiceCollection services)
+        {
+            services.AddTransient<ICreatePasswordHashCommand, CreatePasswordHashCommand>();
+            services.AddTransient<IVerifyPasswordHashCommand, VerifyPasswordHashCommand>();
+            services.AddTransient<ICreateJwtTokenCommand, CreateJwtTokenCommand>();
+            services.AddTransient<ICreateRefreshTokenCommand, CreateRefreshTokenCommand>();
+            services.AddTransient<IGetPrincipalFromExpiredTokenCommand, GetPrincipalFromExpiredTokenCommand>();
+            services.AddTransient<ICreateAccountCommand, CreateAccountCommand>();
+            services.AddTransient<IAuthenticateAccountCommand, AuthenticateAccountCommand>();
+            services.AddTransient<IRefreshTokenCommand, RefreshTokenCommand>();
+
+            services.AddTransient<IAuthenticationService, AuthenticationService>();
+
+            return services;
+        }
+    }
+}
