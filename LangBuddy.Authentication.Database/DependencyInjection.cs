1f42e0fad033a5750791dd4bd03e5718849a6602
FremyXS <antonovch13@mail.ru>
2023-08-20 18:12:14 +0300

    feat(Database): add DependencyInjection

---
+++ LangBuddy.Authentication.Database/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LangBuddy.Authentication.Database
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<AuthenticationDbContext>(options =>
+            {
+                options.UseNpgsql(connectionString,
+                    options => options.UseNodaTime());
+            });
+
+            services.AddScoped<AuthenticationDbContext>();
+
+            return services;
+        }
+    }
+}
