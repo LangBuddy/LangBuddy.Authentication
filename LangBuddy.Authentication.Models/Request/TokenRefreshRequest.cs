11246854f6fdd9b638532c5a2165a892c77aa7db
FremyXS <antonovch13@mail.ru>
2023-08-20 22:01:24 +0300

    feat(Models): add request TokenRefreshRequest

---
+++ LangBuddy.Authentication.Models/Request/TokenRefreshRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LangBuddy.Authentication.Models.Request
+{
+    public record TokenRefreshRequest(string Token, string RefreshToken);
+}
